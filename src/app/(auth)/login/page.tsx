6d876c55d00e697c04e3a7a59b6f07cbba8f10ea
Asraful-code235 <asrafulislamcodes@gmail.com>
2025-06-28 14:07:02 +0600

    updated login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,14 +1,17 @@
-import LoginMain from "@/components/organisms/auth/login/LoginMain";
-import { RedirectBack } from "@/components/organisms/auth/login/RedirectBack";
+import LoginLeft from "@/components/organisms/auth/login/LoginLeft";
+import Image from "next/image";
 import React from "react";
 
 export default function page() {
   return (
-    <div className="w-full min-h-screen flex flex-col items-center justify-center bg-[#f6f6f6]">
-      <div className="hide-scroll container w-full px-4 py-10 sm:p-10 ">
-        <RedirectBack />
-        <LoginMain />
-      </div>
-    </div>
+    <div className="hide-scroll flex h-full min-h-screen w-screen overflow-hidden max-md:justify-end">
+    <Image
+      src={"/login/Loginpagebg.png"}
+      alt="Login Page Background"
+      fill
+      className="absolute inset-0 -z-10 h-full w-full object-cover"
+    />
+    <LoginLeft />
+  </div>
   );
 }
